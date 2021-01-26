851be0ce4926921c03e06efe5360b0a18ae23291
Vladimir Safonkin <vsafonkin@github.com>
2021-01-26 12:35:04 +0300

    Test python3 symlink on Windows

--- src/install-python.ts
+++ src/install-python.ts
@@ -9,7 +9,7 @@ const TOKEN = core.getInput('token');
 const AUTH = !TOKEN || isGhes() ? undefined : `token ${TOKEN}`;
 const MANIFEST_REPO_OWNER = 'actions';
 const MANIFEST_REPO_NAME = 'python-versions';
-const MANIFEST_REPO_BRANCH = 'main';
+const MANIFEST_REPO_BRANCH = 'update-versions-manifest-file';
 export const MANIFEST_URL = `https://raw.githubusercontent.com/${MANIFEST_REPO_OWNER}/${MANIFEST_REPO_NAME}/${MANIFEST_REPO_BRANCH}/versions-manifest.json`;
 
 export async function findReleaseFromManifest(
